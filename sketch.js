fd55e0149b38995c5c4059fa6a66d52ceb73a642
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-10-22 13:32:45 -0500

    TrumpetsActuallyFixed

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(windowWidth/1.2,windowHeight/1.2);
+    createCanvas(1200, 600);
     imageMode(CENTER);
 
     fft=new p5.FFT();
@@ -44,19 +44,19 @@ function draw(){
  
 
     if(addImage1===true){  //top left trumpet
-        image(trumpetImg,200,200,trumpetSize*2,201);
+        image(trumpetImg,200,200,trumpetSize*2,150);
     }
 
     if(addImage2===true){  // bottom left trumpet
-        image(trumpetImg,200,600,trumpetSize*2,201);
+        image(trumpetImg,200,500,trumpetSize*2,150);
     }
     
     if(addImage3===true){  // top right trumpet
-        image(trumpetImg,1400,200,trumpetSize*2,201);
+        image(trumpetImg,1000,200,trumpetSize*2,150);
     }
     
     if(addImage4===true){  // bottom right trumpet
-        image(trumpetImg,1400,600,trumpetSize*2,201);
+        image(trumpetImg,1000,500,trumpetSize*2,150);
     }
 
 }
