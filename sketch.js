a06dfb7d8c3c88bcca2931f20ed9e93634dd3880
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-10-22 11:44:43 -0500

    cuesAdded

--- sketch.js
+++ sketch.js
@@ -1,15 +1,52 @@
-let jamPlay;
+let jamPlay, fft, amp, level, cheeseSize, cheeseImg, trumpetImg, addImage1, addImage2, addImage3, addImage4, cheeseLocation;
 
 function preload(){
-    jamPlay=loadSound('assets/LouieZongTRAFFICJAM.mp3')
+    jamPlay=loadSound('assets/LouieZongTRAFFICJAM.mp3');
+    cheeseImg=loadImage('assets/Cheese.png');
+    trumpetImg=loadImage('assets/trumpet.jpg');
 }
 
 function setup(){
-    createCanvas(windowWidth/2,windowHeight/2);
+    createCanvas(windowWidth/1.2,windowHeight/1.2);
+    imageMode(CENTER);
+
+    amp = new p5.Amplitude();
+    cheeseSize=0;
+
+    jamPlay.addCue(.5,makeImage1)
+    jamPlay.addCue(1.2,makeImage2)
+    jamPlay.addCue(2.3,makeImage3)
+    jamPlay.addCue(3.2,makeImage4)
+
 }
 
 function draw(){
-    background(200);
+    background(220,220,120);
+
+    fill(0)                         // instructions to start
+    textSize(28);
+    text('Press G to start',10,30);
+
+    level=amp.getLevel();
+    cheeseSize=map(level,0,1,0,100);
+    image(cheeseImg,width/2,height/2,cheeseSize*4,cheeseSize*4);
+
+    if(addImage1===true){
+        image(trumpetImg,200,200,230,200);
+    }
+
+    if(addImage2===true){
+        image(trumpetImg,200,600,230,200);
+    }
+    
+    if(addImage3===true){
+        image(trumpetImg,1400,200,230,200);
+    }
+    
+    if(addImage4===true){
+        image(trumpetImg,1400,600,230,200);
+    }
+
 }
 
 
@@ -17,4 +54,19 @@ function keyPressed(){
     if(key === 'g') {
     jamPlay.play();
     }
+}
+
+function makeImage1(){
+    addImage1=true
+}
+function makeImage2(){
+    addImage2=true
+}
+
+function makeImage3(){
+    addImage3=true
+}
+
+function makeImage4(){
+    addImage4=true
 }
