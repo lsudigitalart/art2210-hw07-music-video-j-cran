c6ab878089ef364c73bbd82226ae633ffcbc77ed
Sebastian Crandall <jocelynnecrandall@gmail.com>
2025-10-22 12:35:20 -0500

    trebleAdded

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let jamPlay, fft, amp, level, cheeseSize, cheeseImg, trumpetImg, addImage1, addImage2, addImage3, addImage4, cheeseLocation;
+let jamPlay, fft, amp, level, cheeseSize, trumpetSize, cheeseImg, trumpetImg, addImage1, addImage2, addImage3, addImage4, cheeseLocation;
 
 function preload(){
     jamPlay=loadSound('assets/LouieZongTRAFFICJAM.mp3');
@@ -10,6 +10,7 @@ function setup(){
     createCanvas(windowWidth/1.2,windowHeight/1.2);
     imageMode(CENTER);
 
+    fft=new p5.FFT();
     amp = new p5.Amplitude();
     cheeseSize=0;
 
@@ -23,28 +24,37 @@ function setup(){
 function draw(){
     background(220,220,120);
 
-    fill(0)                         // instructions to start
+    fill(0);
+    textFont("courier new")                   
     textSize(28);
-    text('Press G to start',10,30);
+    text('Press G to start',10,100);
+    textSize(48)
+    text('TRAFFIC JAM - Louie Zong',10,50)
 
+    fft.analyze();
     level=amp.getLevel();
+    let treble=fft.getEnergy('treble');
+
     cheeseSize=map(level,0,1,0,100);
+    trumpetSize=map(treble, 0, 255, 10, 150)
+
     image(cheeseImg,width/2,height/2,cheeseSize*4,cheeseSize*4);
 
+
     if(addImage1===true){
-        image(trumpetImg,200,200,230,200);
+        image(trumpetImg,200,200,trumpetSize*2,200);
     }
 
     if(addImage2===true){
-        image(trumpetImg,200,600,230,200);
+        image(trumpetImg,200,600,trumpetSize*2,200);
     }
     
     if(addImage3===true){
-        image(trumpetImg,1400,200,230,200);
+        image(trumpetImg,1400,200,trumpetSize*2,200);
     }
     
     if(addImage4===true){
-        image(trumpetImg,1400,600,230,200);
+        image(trumpetImg,1400,600,trumpetSize*2,200);
     }
 
 }
